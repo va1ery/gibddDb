de759f5eddcb0c48ab4c467974bbc602a3068e2a
Valery Gurov <va1ery@ya.ru>
2021-04-01 11:04:30 +0300

    Добавьте файлы проекта.

---
+++ gibddDBcore/gibddDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace gibddDBcore
+{
+    public class gibddDbContext : DbContext
+    {
+        public gibddDbContext(DbContextOptions options)
+:              base()
+        {
+        }
+    }
+}
---
+++ gibddDb/Program.cs
@@ -0,0 +1,29 @@
+﻿using gibddDBcore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace gibddDb
+{
+    class Program
+    {
+        static IConfigurationRoot _configuration;
+        static DbContextOptionsBuilder<gibddDbContext> _optionsBuilder;
+        static void Main(string[] args)
+        {
+            BuildOptions();
+            Console.WriteLine(_configuration.GetConnectionString("gibddDBManager"));
+           
+            ListInventory();
+        }
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<gibddDbContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("gibddDBManager"));
+        }
+        static void ListInventory()
+        {
+        }
+    }
+}
