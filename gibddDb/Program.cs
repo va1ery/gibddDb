8ae97835ea51471c1868305006effb47839767a4
Valery Gurov <va1ery@ya.ru>
2021-04-01 12:10:30 +0300

    3 drivers inserted

--- gibddDb/Program.cs
+++ gibddDb/Program.cs
@@ -1,7 +1,10 @@
 ﻿using gibddDBcore;
+using gibddDBModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace gibddDb
 {
@@ -12,9 +15,11 @@ namespace gibddDb
         static void Main(string[] args)
         {
             BuildOptions();
-            Console.WriteLine(_configuration.GetConnectionString("gibddDBManager"));
-           
+            //         Console.WriteLine(_configuration.GetConnectionString("gibddDBManager"));
+            BuildOptions();
+            InsertDrivers();
             ListInventory();
+
         }
         static void BuildOptions()
         {
@@ -24,6 +29,26 @@ namespace gibddDb
         }
         static void ListInventory()
         {
+            using (var db = new gibddDbContext(_optionsBuilder.Options))
+            {
+                var items = db.Drivers.Take(3).OrderBy(x => x.firstName).ToList();
+                items.ForEach(x => Console.WriteLine($"New Driver: {x.firstName}"));
+            }
+        }
+
+        static void InsertDrivers()
+        {
+            var Drivers = new List<Driver>() {
+                new Driver() {firstName = "Alonso", lastName = "Hopkins", middleName = "Caleb",},
+                new Driver() {firstName = "Kaydence", lastName = "Sellers", middleName = "Susannah",},
+                new Driver() {firstName = "Finnegan", lastName = "Bray", middleName = "Brighton",},
+            };
+
+            using (var db = new gibddDbContext(_optionsBuilder.Options))
+            {
+                db.AddRange(Drivers);
+                db.SaveChanges();
+            }
         }
     }
 }
